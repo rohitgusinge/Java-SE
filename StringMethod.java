81f604da24ca41475dfe7f6f5bf5569abd95bf3c
rohitgusinge <79829709+rohitgusinge@users.noreply.github.com>
2021-08-26 13:50:37 +0530

    Update StringMethod.java

--- StringMethod.java
+++ StringMethod.java
@@ -3,7 +3,7 @@ import java.lang.*;
 import java.util.*;
 class StringMethod
 {
-		public static void main(String ars[])
+		public static void main(String args[])
 		{
 			Scanner sc=new Scanner(System.in);
 			String gid;
