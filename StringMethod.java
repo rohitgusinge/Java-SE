dd87de56bedc3dc53a5ee68e2a452ee3bc5e72f7
rohitgusinge <79829709+rohitgusinge@users.noreply.github.com>
2021-08-26 13:55:39 +0530

    Update StringMethod.java

--- StringMethod.java
+++ StringMethod.java
@@ -1,18 +1,18 @@
-//Program to check username and domain from Mail id, if the mail id of g-mail domain then it will print "true" rest "false"
+//Program to check username and domain from Mail id, if the mail id is of g-mail domain then it will print "true" rest "false"
 import java.lang.*;
 import java.util.*;
 class StringMethod
 {
 		public static void main(String args[])
 		{
-			Scanner sc=new Scanner(System.in);
+			Scanner sc=new Scanner(System.in);   //In this program I have accept Mail id from user
 			String gid;
 			System.out.println("Enter your Mail id: "); //Accept Mail id from User
 			gid=sc.nextLine();
 
 
-			//String str="rohit@gmail.com";
-			int i=gid.indexOf("@");         //befor @ it has username
+			//String str="rohit@gmail.com";    //we can also use predefine string
+			int i=gid.indexOf("@");            //befor @ it has username
 			String uname=gid.substring(0,i);
 			String domain=gid.substring(i+1,gid.length());
 
